89f53c46c8c2fd21971a8e4d1dddabf389f98af0
Yatendra-CH24M586 <rrubyrana8@gmail.com>
2026-03-07 19:42:13 +0530

    model loader uodated

--- src/models/model_loader.py
+++ src/models/model_loader.py
@@ -8,14 +8,12 @@ def get_model(model_name, num_classes):
     if model_name == "resnet18":
 
         model = resnet18(weights="DEFAULT", quantize=False)
-        model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
+        model.fc = nn.Linear(model.fc.in_features, num_classes)
 
     elif model_name == "mobilenetv2":
 
         model = mobilenet_v2(weights="DEFAULT")
-        model.classifier[1] = torch.nn.Linear(
-            model.classifier[1].in_features, num_classes
-        )
+        model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
 
     else:
         raise ValueError("Unknown model")
