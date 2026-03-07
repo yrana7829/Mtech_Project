57124881cc21b544ed440aba662f32b81f20d60c
Yatendra-CH24M586 <rrubyrana8@gmail.com>
2026-03-07 19:39:45 +0530

    model loader updated

--- src/models/model_loader.py
+++ src/models/model_loader.py
@@ -1,18 +1,20 @@
-import torch.nn as nn
-from torchvision import models
+from torchvision.models.quantization import resnet18
+from torchvision.models import mobilenet_v2
 
 
 def get_model(model_name, num_classes):
 
     if model_name == "resnet18":
 
-        model = models.resnet18(weights="IMAGENET1K_V1")
-        model.fc = nn.Linear(model.fc.in_features, num_classes)
+        model = resnet18(weights="DEFAULT", quantize=False)
+        model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
 
     elif model_name == "mobilenetv2":
 
-        model = models.mobilenet_v2(weights="IMAGENET1K_V1")
-        model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
+        model = mobilenet_v2(weights="DEFAULT")
+        model.classifier[1] = torch.nn.Linear(
+            model.classifier[1].in_features, num_classes
+        )
 
     else:
         raise ValueError("Unknown model")
