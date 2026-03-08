09118a8e536c41c977af769209a30c9df323415f
Yatendra-CH24M586 <rrubyrana8@gmail.com>
2026-03-08 08:03:11 +0530

    m

--- src/models/model_loader.py
+++ src/models/model_loader.py
@@ -1,20 +1,5 @@
-import torch
 import torch.nn as nn
 import torchvision.models as models
-
-
-class QuantWrapper(nn.Module):
-    def __init__(self, model):
-        super().__init__()
-        self.quant = torch.quantization.QuantStub()
-        self.model = model
-        self.dequant = torch.quantization.DeQuantStub()
-
-    def forward(self, x):
-        x = self.quant(x)
-        x = self.model(x)
-        x = self.dequant(x)
-        return x
 
 
 def get_model(model_name, num_classes):
@@ -28,11 +13,8 @@ def get_model(model_name, num_classes):
 
     elif model_name == "mobilenetv2":
 
-        base = models.mobilenet_v2(weights="DEFAULT")
-        base.classifier[1] = nn.Linear(base.classifier[1].in_features, num_classes)
-
-        # MobileNet requires quant wrapper
-        model = QuantWrapper(base)
+        model = models.mobilenet_v2(weights="DEFAULT")
+        model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
 
         return model
 
