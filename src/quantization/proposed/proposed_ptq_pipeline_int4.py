9f18093b1cf7ac5b4b050c90f6907790d24fbb5d
Yatendra-CH24M586 <rrubyrana8@gmail.com>
2026-03-08 18:53:12 +0530

    int4

--- src/quantization/proposed/proposed_ptq_pipeline_int4.py
+++ src/quantization/proposed/proposed_ptq_pipeline_int4.py
@@ -14,11 +14,11 @@ def apply_proposed_ptq_pipeline_int4(model, device):
     model = apply_proposed_lps(model, device)
     model = wrap_proposed_lps_layers(model)
 
+    print("Step 3: Tail Weighted Clipping")
+    model = apply_proposed_twc(model)
+
     print("Step 2: Mixed Precision INT4")
     model = apply_proposed_mixed_precision_int4(model)
-
-    # print("Step 3: Tail Weighted Clipping")
-    # model = apply_proposed_twc(model)
 
     print("\nPTQ++ INT4 pipeline completed.\n")
 
