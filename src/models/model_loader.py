775573b5c5288903e706c9f7ccf9308bf0887db5
Yatendra-CH24M586 <rrubyrana8@gmail.com>
2026-03-08 07:51:04 +0530

    model loader

--- src/models/model_loader.py
+++ src/models/model_loader.py
@@ -1,21 +1,40 @@
-from torchvision.models.quantization import resnet18
-from torchvision.models import mobilenet_v2
+import torch
 import torch.nn as nn
+import torchvision.models as models
+
+
+class QuantWrapper(nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.quant = torch.quantization.QuantStub()
+        self.model = model
+        self.dequant = torch.quantization.DeQuantStub()
+
+    def forward(self, x):
+        x = self.quant(x)
+        x = self.model(x)
+        x = self.dequant(x)
+        return x
 
 
 def get_model(model_name, num_classes):
 
     if model_name == "resnet18":
 
-        model = resnet18(weights="DEFAULT", quantize=False)
+        model = models.resnet18(weights="DEFAULT")
         model.fc = nn.Linear(model.fc.in_features, num_classes)
+
+        return model
 
     elif model_name == "mobilenetv2":
 
-        model = mobilenet_v2(weights="DEFAULT")
-        model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
+        base = models.mobilenet_v2(weights="DEFAULT")
+        base.classifier[1] = nn.Linear(base.classifier[1].in_features, num_classes)
+
+        # MobileNet requires quant wrapper
+        model = QuantWrapper(base)
+
+        return model
 
     else:
         raise ValueError("Unknown model")
-
-    return model
