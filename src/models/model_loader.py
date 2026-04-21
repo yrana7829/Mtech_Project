f38662a5175d36069fddc6031d3d1b61c880da9f
Yatendra-CH24M586 <rrubyrana8@gmail.com>
2026-04-21 18:08:04 +0530

    modified model loader

--- src/models/model_loader.py
+++ src/models/model_loader.py
@@ -6,14 +6,14 @@ def get_model(model_name, num_classes):
 
     if model_name == "resnet18":
 
-        model = models.resnet18(weights="DEFAULT")
+        model = models.resnet18(weights=None)
         model.fc = nn.Linear(model.fc.in_features, num_classes)
 
         return model
 
     elif model_name == "mobilenetv2":
 
-        model = models.mobilenet_v2(weights="DEFAULT")
+        model = models.mobilenet_v2(weights=None)
         model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
 
         return model
