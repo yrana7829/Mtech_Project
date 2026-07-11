fd9d120acf3b47a53180e5224f8597fb823d3950
Yatendra-CH24M586 <rrubyrana8@gmail.com>
2026-07-11 13:58:11 +0530

    twc v2

--- scripts/diagnosis/checking_Script.py
+++ scripts/diagnosis/checking_Script.py
@@ -5,16 +5,9 @@ import torch
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 sys.path.insert(0, project_root)
 from src.models.model_loader import get_model
-from src.quantization.proposed.proposed_lps_v2 import (
-    apply_proposed_lps_v2,
-    apply_proposed_twc_v2,
-)
-
-# model = get_model("mobilenetv2", num_classes=10)
-
-# model = apply_proposed_lps_v2(model, "cpu")
-
-# print(type(model.features[0][0]))
+from src.models.model_loader import get_model
+from src.quantization.proposed.proposed_lps_v2 import apply_proposed_lps_v2
+from src.quantization.proposed.proposed_twc_v2 import apply_proposed_twc_v2
 
 model = get_model("mobilenetv2", num_classes=10)
 
