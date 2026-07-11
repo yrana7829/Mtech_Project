c58f8d6bbd8fd95d5c040fca9dfa294f37cad64f
Yatendra-CH24M586 <rrubyrana8@gmail.com>
2026-07-11 13:51:04 +0530

    v2

--- scripts/diagnosis/checking_Script.py
+++ scripts/diagnosis/checking_Script.py
@@ -2,44 +2,13 @@ import os
 import sys
 import torch
 
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..\.."))
 
-sys.path.append(project_root)
-
-from src.dataset.dataloader import get_dataset
 from src.models.model_loader import get_model
-from src.evaluation.evaluate import evaluate
+from src.quantization.proposed.proposed_lps_v2 import apply_proposed_lps_v2
 
-# CHECKPOINT_PATH = "results/checkpoints/mobilenet_v2_eurosat_rgb_fp32.pth"
-CHECKPOINT_PATH = "results/checkpoints/eurosat_mobilenetv2_fp32.pth"
-DATASET_NAME = "eurosat"
-NUM_CLASSES = 10
+model = get_model("mobilenetv2", num_classes=10)
 
+model = apply_proposed_lps_v2(model, "cpu")
 
-def main():
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    print("Loading dataset...")
-
-    _, _, test_loader = get_dataset(DATASET_NAME)
-
-    print("Loading model...")
-
-    model = get_model("mobilenetv2", NUM_CLASSES)
-
-    checkpoint = torch.load(CHECKPOINT_PATH, map_location=device)
-
-    model.load_state_dict(checkpoint)
-
-    model.to(device)
-
-    print("Evaluating...")
-
-    acc = evaluate(model, test_loader, device)
-
-    print(f"\nFP32 Checkpoint Accuracy = {acc:.4f}")
-
-
-if __name__ == "__main__":
-    main()
+print(type(model.features[0][0]))
